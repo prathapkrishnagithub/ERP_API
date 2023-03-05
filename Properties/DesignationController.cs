92c951f883ddab2583d3dc1a186d973f54f331a0
Prathap K <prathapkrishnamail@gmail.com>
2023-03-05 16:06:25 +0530

    1

---
+++ Properties/DesignationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ERP_APP.Model;
+
+namespace ERP_APP.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignationController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public DesignationController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Designation
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblDesignation>>> GetTblDesignation()
+        {
+            return await _context.TblDesignation.ToListAsync();
+        }
+
+    }
+}
